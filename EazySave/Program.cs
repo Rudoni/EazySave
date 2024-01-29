88a02593c38dd334f9e98ab7902d4fdfeaded608
EHenquez <enzo.henquez@gmail.com>
2024-01-29 15:28:41 +0100

    Ajout des commentaires du fichier Program.cs

--- EazySave/Program.cs
+++ EazySave/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+//Programme affichant la chaîne de caractères "Hello, World!"
+//Program which displays the "Hello, World!" sentence
 Console.WriteLine("Hello, World!");
 
