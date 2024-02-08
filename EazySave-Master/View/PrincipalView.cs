f292d28010430ffd2350ffca8d73622364177c5c
Rudoni <antoninrudoni@gmail.com>
2024-02-08 21:35:08 +0100

    enum on switch case + PrincipalView

---
+++ EazySave-Master/View/PrincipalView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EazySave_Master.View
+{
+    internal class PrincipalView
+    {
+        public string lang { get; set; }
+        public PrincipalView() { }
+
+        public void update(string lang)
+        {
+            ResourceManager resourceManager = new ResourceManager("EazySave_Master.Languages." + lang, Assembly.GetExecutingAssembly());
+            Console.WriteLine(resourceManager.GetString("Welcome") + "EazySave !");
+            Console.WriteLine(resourceManager.GetString("CreateSave"));
+            Console.WriteLine(resourceManager.GetString("GoSave"));
+            Console.WriteLine(resourceManager.GetString("Settings"));
+
+        }
+    }
+}
